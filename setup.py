418586441a097402e6197c7860c15b2c69e1522e
Alan Alcocer-Iturriza <alan.alcoceriturriza@gmail.com>
2020-10-04 20:12:41 -0600

    Fixed couple things in setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="Schulich-Ignite",  # Replace with your own username
     version="0.0.1",
-    author="Alan Alcocer-Iturriza, Garth Slaney",
-    author_email="author@example.com",
+    author="Radu Schirliu, Alan Alcocer-Iturriza, Garth Slaney",
+    author_email="info@shulichignite.com",
     description="Spark package for Shulich Ignite",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Schulich-Ignite/spark",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
