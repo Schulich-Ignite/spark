8bee6e19fd7c21cc46549548dfaaaacad4a32a8c
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-10-07 20:35:54 -0600

    Update wrappers.py

--- spark/wrappers.py
+++ spark/wrappers.py
@@ -1,1 +1,41 @@
-# other wrappers
+# Commands to run on JupyterLab
+# !pip install ipycanvas
+# !conda install -c conda-forge nodejs
+# !jupyter labextension install @jupyter-widgets/jupyterlab-manager ipycanvas
+
+global canvas
+
+
+def size(w, h):
+    global canvas
+    canvas = Canvas(width=w, height=h)
+
+
+def display():
+    global canvas
+    return canvas
+
+
+def width(w):
+    global canvas
+    canvas.width = w
+
+
+def height(h):
+    global canvas
+    canvas.height = h
+
+
+def clear():
+    global canvas
+    canvas.clear()
+
+
+def rect(x, y, width, height=None):
+    global canvas
+    canvas.fill_rect(x, y, width, height)
+
+
+def stroke_rect(x, y, width, height=None):
+    global canvas
+    canvas.stroke_rect(x, y, width, height)
