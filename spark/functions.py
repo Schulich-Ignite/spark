b3901e43be23fe1163697f6f7e40495be2e709b3
Hesham-E <hesham.elkaliouby@ucalgary.ca>
2020-10-08 21:10:44 -0600

    Add Rect Functions

--- spark/functions.py
+++ spark/functions.py
@@ -1,2 +1,21 @@
 # setup()
 # draw()
+
+global canvas
+def fill_rect (x, y, width, height=None):
+    if (height == None):
+        canvas.fill_rect(x, y, width, width)
+    else:
+        canvas.fill_rect(x, y, width, height)
+
+def stroke_rect (x, y, width, height=None):
+    if (height == None):
+        canvas.stroke_rect(x, y, width, width)
+    else:
+        canvas.stroke_rect(x, y, width, height)
+
+def clear_rect(x, y, width, height=None):
+    if (height == None):
+        canvas.clear_rect(x, y, width, width)
+    else:
+        canvas.clear_rect(x, y, width, height)
