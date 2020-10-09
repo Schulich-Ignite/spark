8af4c803fedbae427aad90603a874484e21b2378
Hesham-E <hesham.elkaliouby@ucalgary.ca>
2020-10-09 14:00:22 -0600

    Fix Syntax on If Statements

--- spark/functions.py
+++ spark/functions.py
@@ -1,21 +1,24 @@
 # setup()
 # draw()
 
-global canvas
+from ipycanvas import *
+
+canvas = Canvas(width = 400, height = 400)
+
 def fill_rect (x, y, width, height=None):
-    if (height == None):
+    if height == None:
         canvas.fill_rect(x, y, width, width)
     else:
         canvas.fill_rect(x, y, width, height)
 
 def stroke_rect (x, y, width, height=None):
-    if (height == None):
+    if height == None:
         canvas.stroke_rect(x, y, width, width)
     else:
         canvas.stroke_rect(x, y, width, height)
 
 def clear_rect(x, y, width, height=None):
-    if (height == None):
+    if height == None:
         canvas.clear_rect(x, y, width, width)
     else:
         canvas.clear_rect(x, y, width, height)
