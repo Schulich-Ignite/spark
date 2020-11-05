dd36ff8eac797eec73408f65d1d75bddda5b2156
Liam Wrubleski <liam@wrubleski.net>
2020-11-04 18:39:37 -0700

    Added key event unit tests, fix issues with 'capitalized' characters

--- spark/util/helper_functions/keyboard_functions.py
+++ spark/util/helper_functions/keyboard_functions.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Dict
 if TYPE_CHECKING:
     from ...core import Core
 
@@ -7,8 +7,32 @@ from functools import reduce
 from operator import and_
 from ..decorators import *
 
+_phys_to_typed = {
+    "Backquote": ('`', '~'),
+    "Digit1": ('1', '!'),
+    "Digit2": ('2', '@'),
+    "Digit3": ('3', '#'),
+    "Digit4": ('4', '$'),
+    "Digit5": ('5', '%'),
+    "Digit6": ('6', '^'),
+    "Digit7": ('7', '&'),
+    "Digit8": ('8', '*'),
+    "Digit9": ('9', '('),
+    "Digit0": ('0', ')'),
+    "Minus": ('-', '_'),
+    "Equal": ('=', '+'),
+    "BracketLeft": ('[', '{'),
+    "BracketRight": (']', '}'),
+    "Backslash": ('\\', '|'),
+    "Semicolon": (';', ':'),
+    "Quote": ('\'', '"'),
+    "Comma": (',', '<'),
+    "Period": ('.', '>'),
+    "Slash": ('/', '?')
+}
 
-def helper_handle_kb_event(self: Core, event):
+
+def helper_handle_kb_event(self: Core, event: Dict[str, str]):
     self.refresh_last_activity()
     key_pressed = self._methods.get("key_pressed", None)
     key_released = self._methods.get("key_released", None)
@@ -24,7 +48,14 @@ def helper_handle_kb_event(self: Core, event):
             if key_repeated:
                 key_repeated()
     else:
-        self._keys_held[self.key] = False
+        if event['code'].isalpha() and len(event['code']) == 1:
+            self._keys_held[event['code'].lower()] = False
+            self._keys_held[event['code'].upper()] = False
+        elif event['code'] in _phys_to_typed:
+            for k in _phys_to_typed[event['code']]:
+                self._keys_held[k] = False
+        else:
+            self._keys_held[self.key] = False
         if key_released:
             key_released()
 
